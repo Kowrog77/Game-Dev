971f1ac0158cedfdfba508d199f0e3596e154721
Koen Rogers <Kowrog77@gmail.com>
2020-02-14 10:04:06 -0600

    d

--- Week One Project/Assets/GameControls.cs
+++ Week One Project/Assets/GameControls.cs
@@ -1,17 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControls : MonoBehaviour {
     public GameObject ballPrefab;
-   
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject ball;
+    public Rigidbody rb;
+    public float speed =0;
+    public float speedMulti = 1;
+    public float Shoot;
+
+    // Use this for initialization
+    void Start () {
+        ball = Instantiate<GameObject>(ballPrefab);
+         rb = ball.GetComponent<Rigidbody>();
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+         Shoot = Input.GetAxis("Shoot");
+        if(rb.velocity.magnitude== 0)
+        {
+            if (Shoot != 0)
+            {
+                speed += speedMulti;
+            }
+            else if (speed != 0)
+            {
+
+                rb.AddForce(transform.right * speed, ForceMode.Impulse);
+
+                speed = 0;
+            }
+        }
+        
+        
+    
+}
+    
 }
